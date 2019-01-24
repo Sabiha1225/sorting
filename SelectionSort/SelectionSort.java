a0d2921826227c57fae0a3aa1c59b2ff548e40a5
Sabiha Afroz <1015052012.sa@grad.cse.buet.ac.bd>
2019-01-24 16:13:54 +0600

    Add files via upload

---
+++ SelectionSort/SelectionSort.java
@@ -0,0 +1,43 @@
+
+package selectionsort;
+
+public class SelectionSort {
+
+    static int[] arr = {11, 25, 12, 22, 64, 3, 5, 11, 23, 26};
+    
+    public static void selectionSort()
+    {
+        for(int i=0; i<(arr.length-1); ++i)
+        {
+            int min = i;
+            for(int j=i+1; j<arr.length; ++j)
+            {
+                if(arr[j] < arr[min])
+                    min = j;
+            }
+            
+            if(min != i)
+            {
+                int t = arr[i];
+                arr[i] = arr[min];
+                arr[min] = t;
+            }
+        }
+    }
+    
+    public static void printArray()
+    {
+        for(int i=0; i<arr.length; ++i)
+        {
+            System.out.print(arr[i] + "  ");
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        selectionSort();
+        printArray();
+    }
+    
+}
